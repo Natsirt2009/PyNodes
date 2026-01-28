8f35f9c43bd62c36eb8f3a6546e4b297c676b352
Natsirt2009 <mail.tristan.franke@gmail.com>
2026-01-28 20:52:20 +0100

    Created Nodes structure

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from nodes import Nodes
 
+if __name__ == '__main__':
+    n = Nodes()
+    n.load("nodes")
+    n.render()
 
 print("In progress!")
---
+++ nodes.py
@@ -0,0 +1,11 @@
+
+
+class Nodes:
+    def __init__(self):
+        pass
+
+    def load(path: str) -> None:
+        pass
+
+    def render() -> None:
+        pass
